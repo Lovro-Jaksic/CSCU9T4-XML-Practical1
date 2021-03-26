074d49d54b33d9ae427ba880da52ea6f52a55856
Lovro-Jaksic <78044682+Lovro-Jaksic@users.noreply.github.com>
2021-03-26 21:08:38 +0100

    Add files via upload

---
+++ MenuHandler.java
@@ -0,0 +1,92 @@
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+/**
+ * SAX event handler to output in basic format.
+ *
+ * @author Lovro Jaksic
+ * @version 20/03/21
+ */
+public class MenuHandler extends DefaultHandler {
+  private String tagName = "";
+
+  /**
+   * Callback when the character data is found
+   * @param ch     character data
+   * @param start  character start index
+   * @param length character count
+   * @throws SAXException
+   */
+  public void characters(char ch[], int start, int length) throws SAXException {
+
+    String characters = new String(ch, start, length).trim();
+
+    if (tagName.equals("description")) {
+      this.tagName="";
+      System.out.printf("%-30s\n", characters);
+    }
+
+    if (tagName.equals("price")) {
+      this.tagName="";
+      System.out.printf("%-30s", characters);
+    }
+
+    if (tagName.equals("name")) {
+      this.tagName="";
+      System.out.printf("%-30s", characters);
+    }
+
+  }
+  /**
+   Callback when parser finds the end of a document.
+
+   @throws SAXException
+   */
+  public void endDocument () throws SAXException {
+
+  }
+
+  /**
+   Callback when parser finds the end of an element.
+
+   @param namespaceURI        namespace URI
+   @param localName        local namespace identifier
+   @param qName        qualified name for namespace
+   @throws SAXException
+   */
+  public void endElement (String namespaceURI, String localName, String qName)
+          throws SAXException {
+//    System.err.println("endElement callback for '" + qName + "' '" + localName +"'");
+  }
+
+  /**
+   Callback when parser starts to read a document.
+
+   @throws SAXException
+   */
+  public void startDocument () throws SAXException {
+    System.err.printf("%-30s%-30s%-30s\n", "Name", "Price", "Description");
+  }
+
+  /**
+   Callback when parser starts to read an element.
+
+   @param namespaceURI        namespace URI
+   @param localName        local namespace identifier
+   @param qName        qualified name for namespace
+   @param attributes        elements attributes
+   @throws SAXException
+   */
+  public void startElement (String namespaceURI, String localName,
+                            String qName, Attributes attributes) throws SAXException {
+    this.tagName = qName;
+
+//    System.err.println("startElement callback for '"  + qName + "'");
+//    for (int i = 0; i < attributes.getLength(); i++)
+//      System.err.println(
+// "  attribute '" + attributes.getQName(i) +
+// "' is '" + attributes.getValue(i) + "'");
+  }
+
+}
---
+++ SAXMenu.java
@@ -0,0 +1,106 @@
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+
+import javax.xml.XMLConstants;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParserFactory;
+import javax.xml.transform.sax.SAXSource;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+/**
+ * SAX event handler to:
+ * <p>
+ * 1) Show basic event handling while reading an XML file
+ * 2) output in a csv file
+ *
+ * @author Lovro Jaksic
+ * @version 20/03/21
+ */
+public class SAXMenu {
+
+  /**
+   * Main program to call SAX parser.
+   *
+   * @param args command-line arguments
+   * First argument is the name of the xml file to process
+   */
+
+
+  public static void main(String[] args) {
+    //if validation is true proceed to parsing
+    if (validateDocument(args[0],args[1])){
+      parse(args[0]);
+    }
+    else{
+      System.err.println("File could not be parsed");
+    }
+
+  }
+
+  /**
+   * Callback when parser finds character data.
+   *
+   * @param filename XML file to read
+   */
+  private static void parse(String filename) {
+    try {
+      System.out.println("-------------------");
+      System.out.println("parsing " + filename);
+      System.out.println();
+      // get an instance of SAXParserFactory and get an XMLReader from it
+      SAXParserFactory factory = SAXParserFactory.newInstance();
+
+      XMLReader reader = factory.newSAXParser().getXMLReader();
+      // turn off XML validation
+      reader.setFeature("http://xml.org/sax/features/validation", false);
+      // register the relevant handler with the parser, choosing one of:
+      MenuHandler handler = new MenuHandler();
+      //CountHandler handler = new CountHandler();
+      reader.setContentHandler(handler);
+      reader.setErrorHandler(handler);
+      // parse the given file
+
+
+      InputSource inputSource = new InputSource(filename);
+      reader.parse(inputSource);
+
+      System.out.println("-------------------");
+      System.out.println();
+    } catch (SAXException saxException) {
+      System.err.println("There is a mistake in the parsing- " + saxException);
+    } catch (IOException op) {
+      System.err.println("The file to parse is not found -" + op.getMessage());
+    } catch (ParserConfigurationException ex) {
+      System.err.println("The file cannot be configured- " + ex.getMessage());
+    }
+  }
+
+  private static Boolean validateDocument(String xmlFilename,String xsdFilename) {
+    try {
+      SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+      //create the schema that uses the xsd as if the file is correct
+      Schema schema = schemaFactory.newSchema(new StreamSource(xsdFilename));
+      Validator validator = schema.newValidator();
+      //take the file to validate with the xsd
+      FileInputStream fileInputStream = new FileInputStream(xmlFilename);
+      InputSource inputSource = new InputSource(fileInputStream);
+      //pass it as the Sax source
+      SAXSource source = new SAXSource(inputSource);
+      //validate
+      validator.validate(source);
+      return true;// return true to parse the xml because there are no errors
+
+    } catch (SAXException | IOException exception) {
+      System.err.println(exception.getMessage());
+      return false;  // return false if there is an exception
+    }
+
+
+  }
+}
